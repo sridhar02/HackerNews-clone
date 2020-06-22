e65a39df3b0f0c4756beb94e239be4bbaa7c409e
sridhar02 <kattasridhar02@gmail.com>
2020-06-22 15:46:46 +0530

    removed console log

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -13,7 +13,6 @@ export default function StoriesContainer() {
 
   useEffect(() => {
     getStoryIds().then((data) => setStoryIds(data));
-    console.log("count" + count);
   }, [count]);
   return (
     <>
